6d00cd7e8a11cdb367a342a106cf44e2bcfed225
shyam1234 <malviya.software@gmail.com>
2017-06-04 11:29:36 +0530

    office

--- app/src/main/java/com/stpl/edurp/activities/LoginActivity.java
+++ app/src/main/java/com/stpl/edurp/activities/LoginActivity.java
@@ -179,6 +179,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             });
             //------------------------------------------------
 
+        }else{
+            reset();
         }
     }
 
